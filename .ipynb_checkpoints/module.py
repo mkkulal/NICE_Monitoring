40716fbb0872e7b2f1faa5aadf50cf32d0218089
mkulal <mithun.kulal@nice.com>
2022-09-29 00:31:15 +0530

    add more files

---
+++ .ipynb_checkpoints/module.py
@@ -0,0 +1,24 @@
+import calendar
+from cgitb import reset
+from unittest import result
+cal = calendar.month(2022, 9)
+print (cal)
+
+import math
+result = math.sqrt(25)
+print (result)
+
+import random
+rand = random.randint(1,999)
+print(rand)
+
+movies = ["Movie","Movie5","Movie2" ]
+watch = random.choice(movies)
+print (watch)
+
+deck = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nice", "ten", "Jack", "Queen", "King"]
+random.shuffle(deck)
+print(deck)
+
+result = math.pow(2,6)
+print(result)
